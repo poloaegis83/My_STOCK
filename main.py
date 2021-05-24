8b623a1786cacae3aca4b43ea8b79c70dab1f9cc
poloaegis83 <84704074+poloaegis83@users.noreply.github.com>
2021-05-24 17:11:11 +0800

    Add function to draw the chart of Stock
    Add function to draw the chart of Stock

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import requests
-url = "https://goodinfo.tw/StockInfo/StockBzPerformance.asp?STOCK_ID=2330"
-res = requests.get(url)
-res.encoding = "utf-8"
-res.text
+import pandas as pd
+import json
+import numpy as np
+import datetime
+
+def crawl_price(stock_id):
+
+    d = datetime.datetime.now()
+    url = "https://query1.finance.yahoo.com/v8/finance/chart/"+stock_id+"?period1=0&period2="+str(int(d.timestamp()))+"&interval=1d&events=history&=hP2rOschxO0"
+
+    res = requests.get(url)
+    data = json.loads(res.text)
+    df = pd.DataFrame(data['chart']['result'][0]['indicators']['quote'][0], index=pd.to_datetime(np.array(data['chart']['result'][0]['timestamp'])*1000*1000*1000))
+    return df
+
+df = crawl_price("2330.TW")
+df.close.plot()
