fdc268517ceaa2e507fc78734504c38d94466f91
poloaegis83 <84704074+poloaegis83@users.noreply.github.com>
2021-05-30 10:44:21 +0800

    Update an example to generate XML file
    An example to generate XML file

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 import json
 import numpy    as np
 import pandas   as pd
+import xml.etree.cElementTree as ET
+import lxml.etree as etree
 from   bs4      import BeautifulSoup
 
 headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
@@ -32,20 +34,59 @@ print(title_tag)
 
 a_tags = soup.find_all('stockalllistbg1')
 
-for tag in a_tags:
+#for tag in a_tags:
   # 輸出超連結的文字
-  print(tag.string)
+ # print(tag.string)
 
 link2_tag = soup.find_all(class_='stockalllistbg1')
-print(link2_tag)
+#print(link2_tag)
 
 link2_tag = soup.find_all(class_='stockalllistbg2')
-print(link2_tag)
+#print(link2_tag)
 
-for tag in link2_tag:
+#for tag in link2_tag:
   # 輸出超連結的文字
-  print(tag.string)
+ # print(tag.string)
 
 
-os.mknod('test.xml')
-os.remove('test.xml')
+#os.mknod('test.xml')
+
+
+StockData = ET.Element('StockData',Version="1.0")
+StockId = ET.SubElement(StockData, 'StockId')
+Daily = ET.SubElement(StockData, 'Daily', Index="0",Years="0000",Months="00",Days="00")
+Price = ET.SubElement(Daily, 'Price', Start="0",End="0",High="0",Low="0")
+Difference = ET.SubElement(Daily, 'Difference',LeaderDiff="0",ForeignInvestorsDiff="0",InvestmentTrustDiff="0",DealersDiff="0")
+
+StockId.text = "Stock ID"
+
+Daily.set('Index','1')
+Daily.set('Years','2021')
+Daily.set('Months','01')
+Daily.set('Days','01')
+
+Price.set('Low','money')
+Price.set('High','money')
+Price.set('End','money')
+Price.set('Start','money')
+
+Difference.set('DealersDiff','Diff')
+Difference.set('InvestmentTrustDiff','Diff')
+Difference.set('ForeignInvestorsDiff','Diff')
+Difference.set('LeaderDiff','Diff')
+
+tree = ET.ElementTree(StockData)
+tree.write("test.xml",xml_declaration=True,encoding='UTF-8',method="xml")
+
+# 讀取 XML 檔案
+root = etree.parse("test.xml")
+
+# 輸出排版的 XML 資料
+print(etree.tostring(root, pretty_print=True, encoding="unicode"))
+
+# 將排版的 XML 資料寫入檔案
+root.write("test.xml", encoding="utf-8")
+
+ET.dump(StockData)
+
+#os.remove('test.xml')
