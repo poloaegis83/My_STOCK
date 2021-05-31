d4b9655b6ada046308e8bcdef36efe3e30c44027
poloaegis83 <84704074+poloaegis83@users.noreply.github.com>
2021-05-31 15:46:27 +0800

    Update easier for loop control
    Update easier for loop control

--- main.py
+++ main.py
@@ -13,13 +13,10 @@ headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 #print(res.apparent_encoding) #自動判斷字符集類型
 #os.mknod('test.xml')
 
-#stock_id = input ("請輸入股票代碼: ")
-#year = input ("請輸入year: ")
-#month = input ("請輸入mouth: ")
+stock_id = input ("請輸入股票代碼: ")
+year = input ("請輸入year: ")
+month = input ("請輸入mouth: ")
 
-stock_id = '2330'
-year = '2020'
-month = '02'
 print(stock_id)
 year = str(int(year)-1911)
 
@@ -32,8 +29,8 @@ Soup   = ['','','']
 Tables = ['','','']
 tab    = ['','','']
 
-for i in range(3):
-    print(UrlArr[i])
+for i in range(3):  # Dump all 3 urls data
+    #print(UrlArr[i])
     res = requests.get (UrlArr[i], headers = headers)
     res.encoding = 'cp950'
     Soup[i] = BeautifulSoup (res.text, "html.parser")
@@ -46,63 +43,49 @@ StockData = ET.Element('StockData',Version="1.0")
 StockId = ET.SubElement(StockData, 'StockId')
 StockId.text = stock_id
 
-i = 0
-j = 0
+i = 0 # Daily index
+j = 0 # tab[0] td index
+k = 0 # tab[1] td index
+l = 0 # tab[2] td index
 length = len(tab[0].findAll('tr'))
 
-for tr in tab[0].findAll('tr'):  #tab[0] = 股價
-    if i >= 2:
-        #Daily = ET.SubElement(StockData, 'Daily') # Index="0",Years="0000",Months="00",Days="00")
+for tr1,tr2,tr3 in zip(tab[0].findAll('tr'), tab[1].findAll('tr'), tab[2].findAll('tr')):
+    if i >= 2:  # 新增 daily element
         Daily = ET.Element('Daily')
         StockData.insert(1,Daily)
         Daily.set('Index',str(length-i))
         Price = ET.SubElement(Daily, 'Price')
         Difference = ET.SubElement(Daily, 'Difference',LeaderDiff="0",ForeignInvestorsDiff="0",InvestmentTrustDiff="0",DealersDiff="0") # Will uptade in tab[1] and tab[2]
-    for td in tr.findAll('td'):
-        if i >= 2 and j % 6 == 1:  #Time
-            Daily.set('Years',str(int(td.getText()[0:3])+1911))
-            Daily.set('Months',td.getText()[4:6])
-            Daily.set('Days',td.getText()[7:9])
-        if i >= 2 and j % 6 == 2:  #Start
-            Price.set('Start',(td.getText().rstrip()).replace('.',"") )  # replace '.' to '' and remove right space
-        if i >= 2 and j % 6 == 3:  #High
-            Price.set('High',(td.getText().rstrip()).replace('.',"") )
-        if i >= 2 and j % 6 == 4:  #Low
-            Price.set('Low',(td.getText().rstrip()).replace('.',"") )
-        if i >= 2 and j % 6 == 5:  #End
-            Price.set('End',(td.getText().rstrip()).replace('.',"") )
-        j += 1
-        #print('i=',i,'j=',j)
-    i += 1
 
-i = 0
-j = 0
-DailyD = StockData.findall('Daily')
-length = len(DailyD)
-
-for tr in tab[1].findAll('tr'):  #tab[1] = 三大法人
-    for td in tr.findAll('td'):
+    for td in tr1.findAll('td'): # 股價
         if i >= 2:
-            DiffAll = DailyD[length+1-i].find('Difference')
-            if j % 4 == 2:  # 投信
-                DiffAll.set('InvestmentTrustDiff',(td.getText().rstrip()).replace(',',"") )
-            if j % 4 == 3:  # 自營商
-                DiffAll.set('DealersDiff',(td.getText().rstrip()).replace(',',""))
-            if j % 4 == 0:  # 外資
-                DiffAll.set('ForeignInvestorsDiff',(td.getText().rstrip()).replace(',',"") )
+            if j % 6 == 1:  # 日期
+                Daily.set('Years',str(int(td.getText()[0:3])+1911))
+                Daily.set('Months',td.getText()[4:6])
+                Daily.set('Days',td.getText()[7:9])
+            if j % 6 == 2:  # 開盤
+                Price.set('Start',(td.getText().rstrip()).replace('.',"") ) 
+            if j % 6 == 3:  # 最高
+                Price.set('High',(td.getText().rstrip()).replace('.',"") )
+            if j % 6 == 4:  # 最低
+                Price.set('low'',(td.getText().rstrip()).replace('.',"") )              
+            if j % 6 == 5:  # 收盤             
+                Price.set('End',(td.getText().rstrip()).replace('.',"") )
         j += 1
-    i += 1
-
-
-i = 0
-j = 0
-for tr in tab[2].findAll('tr'):  #tab[2] = 主力庫存
-    for td in tr.findAll('td'):
+    for td in tr2.findAll('td'):  # 三大法人
         if i >= 2:
-            DiffAll = DailyD[length+1-i].find('Difference')
-            if j % 3 == 0:  # 主力增減
-                DiffAll.set('LeaderDiff',(td.getText().rstrip()).replace(',',"") )
-        j += 1
+            if k % 4 == 2:  # 投信
+                Difference.set('InvestmentTrustDiff',(td.getText().rstrip()).replace(',',"") )
+            if k % 4 == 3:  # 自營商
+                Difference.set('DealersDiff',(td.getText().rstrip()).replace(',',""))
+            if k % 4 == 0:  # 外資
+                Difference.set('ForeignInvestorsDiff',(td.getText().rstrip()).replace(',',"") )
+        k += 1
+    for td in tr3.findAll('td'): # 主力進出
+        if i >= 2:
+            if l % 3 == 0:  # 主力增減
+                Difference.set('LeaderDiff',(td.getText().rstrip()).replace(',',"") )
+        l += 1
     i += 1
 
 # XML 檔案
