c04fa078c5379b2efea6e74bc053c0903667316b
poloaegis83 <84704074+poloaegis83@users.noreply.github.com>
2021-05-31 15:21:09 +0800

    Example for getting all the data to XML
    Example for getting all the data to XML

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 #print(res.apparent_encoding) #自動判斷字符集類型
 #os.mknod('test.xml')
 
-stock_id = input ("請輸入股票代碼: ")
-year = input ("請輸入year: ")
-month = input ("請輸入mouth: ")
+#stock_id = input ("請輸入股票代碼: ")
+#year = input ("請輸入year: ")
+#month = input ("請輸入mouth: ")
+
+stock_id = '2330'
+year = '2020'
+month = '02'
 print(stock_id)
 year = str(int(year)-1911)
 
@@ -48,13 +52,15 @@ length = len(tab[0].findAll('tr'))
 
 for tr in tab[0].findAll('tr'):  #tab[0] = 股價
     if i >= 2:
-        Daily = ET.SubElement(StockData, 'Daily') # Index="0",Years="0000",Months="00",Days="00")
+        #Daily = ET.SubElement(StockData, 'Daily') # Index="0",Years="0000",Months="00",Days="00")
+        Daily = ET.Element('Daily')
+        StockData.insert(1,Daily)
         Daily.set('Index',str(length-i))
         Price = ET.SubElement(Daily, 'Price')
         Difference = ET.SubElement(Daily, 'Difference',LeaderDiff="0",ForeignInvestorsDiff="0",InvestmentTrustDiff="0",DealersDiff="0") # Will uptade in tab[1] and tab[2]
     for td in tr.findAll('td'):
         if i >= 2 and j % 6 == 1:  #Time
-            Daily.set('Years',td.getText()[0:3])
+            Daily.set('Years',str(int(td.getText()[0:3])+1911))
             Daily.set('Months',td.getText()[4:6])
             Daily.set('Days',td.getText()[7:9])
         if i >= 2 and j % 6 == 2:  #Start
@@ -71,43 +77,33 @@ for tr in tab[0].findAll('tr'):  #tab[0] = 股價
 
 i = 0
 j = 0
+DailyD = StockData.findall('Daily')
+length = len(DailyD)
+
 for tr in tab[1].findAll('tr'):  #tab[1] = 三大法人
     for td in tr.findAll('td'):
-        #if i >= 2:
-            #if j % 4 = 2:  # 投信
-            #if j % 4 = 3:  # 自營商
-            #if j % 4 = 0:  # 外資
-        print('i=',i,'j=',j)
-        print(td.getText())
+        if i >= 2:
+            DiffAll = DailyD[length+1-i].find('Difference')
+            if j % 4 == 2:  # 投信
+                DiffAll.set('InvestmentTrustDiff',(td.getText().rstrip()).replace(',',"") )
+            if j % 4 == 3:  # 自營商
+                DiffAll.set('DealersDiff',(td.getText().rstrip()).replace(',',""))
+            if j % 4 == 0:  # 外資
+                DiffAll.set('ForeignInvestorsDiff',(td.getText().rstrip()).replace(',',"") )
         j += 1
-    i += 1      
+    i += 1
 
 
 i = 0
 j = 0
 for tr in tab[2].findAll('tr'):  #tab[2] = 主力庫存
     for td in tr.findAll('td'):
-        #if i >= 2:
-            #if j % 4 = 0:  # 主力增減
-        print('i=',i,'j=',j)
-        print(td.getText())
+        if i >= 2:
+            DiffAll = DailyD[length+1-i].find('Difference')
+            if j % 3 == 0:  # 主力增減
+                DiffAll.set('LeaderDiff',(td.getText().rstrip()).replace(',',"") )
         j += 1
     i += 1
-
-#Daily.set('Index','1')
-#Daily.set('Years','2021')
-#Daily.set('Months','01')
-#Daily.set('Days','01')
-
-#Price.set('Low','money')
-#Price.set('High','money')
-#Price.set('End','money')
-#Price.set('Start','money')
-
-#Difference.set('DealersDiff','Diff')
-#Difference.set('InvestmentTrustDiff','Diff')
-#Difference.set('ForeignInvestorsDiff','Diff')
-#Difference.set('LeaderDiff','Diff')
 
 # XML 檔案
 tree = ET.ElementTree(StockData)
